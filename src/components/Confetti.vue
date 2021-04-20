8b42d5db3480448cf8d0e0c73f71dee255363033
Anri Lombard <anri.m.lombard@gmail.com>
2021-04-20 15:50:36 +0200

    Better celebration + fully responsive I think

--- src/components/Confetti.vue
+++ src/components/Confetti.vue
@@ -1,9 +1,9 @@
 <template>
   <lottie-player
-    src="https://assets6.lottiefiles.com/packages/lf20_30iie6.json"
+    src="https://assets8.lottiefiles.com/datafiles/6lq0JF4GbfKULT1/data.json"
     background="transparent"
     speed="1"
-    style="width: 100%; height: 100%;"
+    style="width: 100%; height: 100%; margin-top: 0"
     loop
     autoplay
   ></lottie-player>
